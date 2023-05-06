52cfb96fe55dd8593b8c0111d21761f5a52d823e
jfrasinska <j.frasinska@gmail.com>
2023-05-06 21:14:29 +0200

    adding first part of homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const gallery = galleryItems.map((item) => {
+  return `<div class="gallery__item">
+              <a class="gallery__link" href="${item.original}">
+                <img
+                  class="gallery__image"
+                  src="${item.preview}"
+                  data-source="${item.original}"
+                  alt="${item.description}"
+                />
+              </a>
+            </div>`;
+});
+
+galleryContainer.insertAdjacentHTML("beforeend", gallery.join(""));
+
+galleryContainer.addEventListener("click", handleGalleryClick);
+
+function handleGalleryClick(event) {
+  event.preventDefault();
+
+  const clickedElement = event.target;
+  if (clickedElement.tagName === "IMG") {
+    const largeImageURL = clickedElement.dataset.source;
+    const instance = basicLightbox.create(
+      `<img src="${largeImageURL}" alt="Full Image">`
+    );
+
+    instance.show();
+  }
+}
