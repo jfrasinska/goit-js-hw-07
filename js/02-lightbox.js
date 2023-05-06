d6f38492a958e774096d168c39ebb4c4d1799eb5
jfrasinska <j.frasinska@gmail.com>
2023-05-06 21:40:57 +0200

    adding second part of homework

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+const gallery = galleryItems.map((item) => {
+  return `<li>
+              <a class="gallery__item" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+              </a>
+            </li>`;
+});
+
+galleryContainer.insertAdjacentHTML("beforeend", gallery.join(""));
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
